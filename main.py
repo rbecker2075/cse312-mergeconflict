fb0f9656eaa00eacf4514d6ba903c1c4d5e70b22
Shamil S Uddin <shamilud@buffalo.edu>
2025-04-18 15:38:45 -0400

    basic homepage

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
+from fastapi.responses import FileResponse
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
-app = FastAPI()
+app = FastAPI(title='test',
+              description='testing desc',
+              version='1.0')
 
+app.mount("/", StaticFiles(directory="public", html=True), name="static")
 
+INDEX_FILE_PATH = os.path.join("public", "index.html")
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return FileResponse(INDEX_FILE_PATH)
 
 
 @app.get("/hello/{name}")
