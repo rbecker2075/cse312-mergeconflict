11785450ec1ed4271317b0422c91bcce158485dd
EBlessingP <emmablessing2004@gmail.com>
2025-04-22 11:05:42 -0400

    Updated readme

--- README.md
+++ README.md
@@ -9,8 +9,14 @@
 * Agar.io clone
 
 ### Current Goals:
+* phaser.io
+* game implementation 
 
+### Next Goals
+* Deployment 
+
+### Past Goals:
 * DockerCompose -- started, have the docker-compose file to successfully build basic app and database
 * Database -- have database running in docker, currently not sure if in seperate or same object but will fix if wrong in next commit
-* Authentication
-* Logging -- started, to use place Request and Response object into parameter of path and pass to function in logging
+* Authentication 
+* Logging -- moved to main, using middleware
--- logging.py
+++ logging.py
@@ -1,16 +1,1 @@
-from fastapi import FastAPI , Request, Response
-import datetime
-
-def request_log(request : Request, response : Response ):
-   time = datetime.now()
-   content = time.isoformat() + "\n client" + request.client+host+"\n method" + request.method + "\n url path" + request.url.path + '\n response code' + Response.status_code 
-   with open("request_logs.txt","a") as f:
-        f.write(content)
-
-#to do docker logging, volume
-
-# full request and response logging 
-
-#errors 
-
-#registration/loging errors 
+#need to delete this file logging stuff got moved to main so it can handle middleware
