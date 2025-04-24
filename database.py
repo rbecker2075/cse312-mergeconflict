76500289467d3935a1b83d94d78fdef6139e0869
rbecker2075 <rrbecker@buffalo.edu>
2025-04-24 19:13:23 -0400

    Updated Ports

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import os
 
 # Database connection
-client = MongoClient(os.environ.get("MONGO_URL", "mongodb://localhost:27017"))
+client = MongoClient(os.environ.get("MONGO_URL", "mongodb://localhost:20420"))
 db = client.app_database
 users_collection = db.users
 sessions_collection = db.sessions 
